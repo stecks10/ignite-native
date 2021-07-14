546d45376168642314c3f393e429e895c08c17ca
Vitor Nunes <stecks10@gmail.com>
2021-07-14 07:38:35 -0300

    Utilizando StyleSheet

--- projeto/myskills/src/pages/Home.js
+++ projeto/myskills/src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>React Native</Text>
+      <Text style={styles.title}>Welcome, Vitor</Text>
 
-      <Text>Ignite</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='New skill'
+        placeholderTextColor='#555'
+      />
+
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>My Skills</Text>
     </View>
   );
 }
@@ -14,7 +31,34 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30,
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
     alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
   },
 });
