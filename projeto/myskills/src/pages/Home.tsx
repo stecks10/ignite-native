39ceee54722974055f3dd319d5f5ac1d89340e05
Vitor Nunes <stecks10@gmail.com>
2021-07-16 16:48:34 -0300

    Finalizando o nosso App

--- projeto/myskills/src/pages/Home.tsx
+++ projeto/myskills/src/pages/Home.tsx
@@ -29,6 +29,10 @@ export function Home() {
     setMySkills((oldState) => [...oldState, data]);
   }
 
+  function handleRemoveSkill(id: string) {
+    setMySkills((oldState) => oldState.filter((skill) => skill.id !== id));
+  }
+
   useEffect(() => {
     const currentHour = new Date().getHours();
 
@@ -52,13 +56,19 @@ export function Home() {
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
-      <Button onPress={handleAddNewSkill} />
+
+      <Button title="Add" onPress={handleAddNewSkill} />
       <Text style={[styles.title, { marginVertical: 50 }]}>MySkills</Text>
 
       <FlatList
         data={mySkills}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <SkillCard skill={item.name} />}
+        renderItem={({ item }) => (
+          <SkillCard
+            skill={item.name}
+            onPress={() => handleRemoveSkill(item.id)}
+          />
+        )}
       />
     </View>
   );
@@ -68,7 +78,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121015',
-    paddingHorizontal: 20,
     paddingVertical: 70,
     paddingHorizontal: 30,
   },
